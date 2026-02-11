ae699ef34458e8e60540c483f67240ba5036bf31
fantasystar94 <aniseed_3@naver.com>
2026-02-11 21:01:07 +0900

    feat : baseurl 변경

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 
-const awsUrl = "http://43.201.115.73:8080";
-const _localUrl = "http://localhost:8080";
-const baseURL = awsUrl;
+// const awsUrl = "http://43.201.115.73:8080";
+// const _localUrl = "http://localhost:8080";
+const baseURL = "/api";
 export const api = axios.create({
   baseURL,
   withCredentials: true,
