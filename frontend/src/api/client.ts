3dab08b151a53e7e1c1600de42df7b15863450e9
fantasystar94 <aniseed_3@naver.com>
2026-02-19 17:23:56 +0900

    리다이렉트 url 수정

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 
 // const _awsUrl = "http://43.201.115.73:8080";
 // const localUrl = "http://localhost:8080";
-
+const prod = "/api";
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: prod,
   withCredentials: true,
 });
 
